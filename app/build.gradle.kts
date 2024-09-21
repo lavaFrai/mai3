9297be32cac2265b8f3825ba10e4d5d68f6fdafb
Evgeny Meltsaykin <mel.ea@skbkontur.ru>
2024-09-22 00:11:02 +0500

    Remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,9 +61,6 @@ android {
     buildFeatures {
         compose = true
     }
-    /*composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }*/
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -86,11 +83,6 @@ dependencies {
     implementation(libs.androidx.glance.appwidget)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.browser)
-    testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
